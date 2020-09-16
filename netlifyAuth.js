6092d9ef70b9b260bd80dc74052edbae750c2374
hellobrian <brianhan@hey.com>
2020-09-16 17:24:33 -0500

    move on close handler inside on login handler

--- netlifyAuth.js
+++ netlifyAuth.js
@@ -17,10 +17,10 @@ const netlifyAuth = {
       this.user = user;
       callback(user);
       netlifyIdentity.close();
-    });
-    netlifyIdentity.on("close", () => {
-      console.log("close");
-      window.location.reload();
+      netlifyIdentity.on("close", () => {
+        console.log("close");
+        window.location.reload();
+      });
     });
   },
   signout(callback) {
