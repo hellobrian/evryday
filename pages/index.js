b9aa3aea63ea7d482c819c8e46738a416da773e9
hellobrian <brianhan@hey.com>
2020-09-16 21:14:21 -0500

    setup eslint, prettier and update useAuth to use reducer

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
-import { useAuth } from "../utils/useAuth";
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import { useAuth } from '../utils/useAuth';
 
 export default function Home() {
   const { login, logout, user, loggedIn } = useAuth();
@@ -34,19 +34,25 @@ export default function Home() {
         </div>
 
         <p className={styles.description}>
-          Get started by editing{" "}
+          Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <p>
+              Find in-depth information about Next.js features and
+              API.
+            </p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
             <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+            <p>
+              Learn about Next.js in an interactive course with
+              quizzes!
+            </p>
           </a>
 
           <a
@@ -54,7 +60,10 @@ export default function Home() {
             className={styles.card}
           >
             <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <p>
+              Discover and deploy boilerplate example Next.js
+              projects.
+            </p>
           </a>
 
           <a
@@ -63,7 +72,8 @@ export default function Home() {
           >
             <h3>Deploy &rarr;</h3>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Instantly deploy your Next.js site to a public URL with
+              Vercel.
             </p>
           </a>
         </div>
@@ -75,8 +85,12 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          Powered by{' '}
+          <img
+            src="/vercel.svg"
+            alt="Vercel Logo"
+            className={styles.logo}
+          />
         </a>
       </footer>
     </div>
--- utils/useAuth.js
+++ utils/useAuth.js
@@ -1,66 +1,59 @@
-import { useEffect, useState } from "react";
-import netlifyIdentity from "netlify-identity-widget";
+import { useEffect, useReducer } from 'react';
+import { netlifyAuth } from './netlifyAuth';
 
-export const netlifyAuth = {
-  isAuthenticated: false,
-  user: null,
-  initialize(callback) {
-    window.netlifyIdentity = netlifyIdentity;
-    netlifyIdentity.on("init", (user) => {
-      callback(user);
-    });
-    netlifyIdentity.init();
-  },
-  authenticate(callback) {
-    this.isAuthenticated = true;
-    netlifyIdentity.open();
-    netlifyIdentity.on("login", (user) => {
-      this.user = user;
-      callback(user);
-      netlifyIdentity.close();
-    });
-  },
-  signout(callback) {
-    this.isAuthenticated = false;
-    netlifyIdentity.logout();
-    netlifyIdentity.on("logout", () => {
-      this.user = null;
-      callback();
-    });
-  },
-};
+function authReducer(_, action) {
+  switch (action.type) {
+    case 'init':
+    case 'login': {
+      return {
+        loggedIn: !!action.payload,
+        user: action.payload,
+      };
+    }
+    case 'logout': {
+      return { loggedIn: false, user: null };
+    }
+    default: {
+      throw new Error(`Unhandled type: ${action.type}`);
+    }
+  }
+}
 
 export function useAuth() {
-  let [loggedIn, setLoggedIn] = useState(netlifyAuth.isAuthenticated);
-  let [user, setUser] = useState(null);
+  const [state, dispatch] = useReducer(authReducer, {
+    loggedIn: netlifyAuth.isAuthenticated,
+    user: null,
+  });
 
   useEffect(() => {
-    let isCurrent = true;
+    // let isCurrent = true;
     netlifyAuth.initialize((user) => {
-      if (isCurrent) {
-        setLoggedIn(!!user);
-        setUser(user);
-      }
+      // if (isCurrent) {
+      dispatch({ type: 'init', payload: user });
+      // }
     });
 
-    return () => {
-      isCurrent = false;
-    };
+    // return () => {
+    //   isCurrent = false;
+    // };
   }, []);
 
-  let login = () => {
+  const login = () => {
     netlifyAuth.authenticate((user) => {
-      setLoggedIn(!!user);
-      setUser(user);
+      dispatch({ type: 'login', payload: user });
     });
   };
 
-  let logout = () => {
+  const logout = () => {
     netlifyAuth.signout(() => {
-      setLoggedIn(false);
-      setUser(null);
+      dispatch({ type: 'logout', user: null });
     });
   };
 
-  return { login, logout, user, loggedIn };
+  return {
+    login,
+    logout,
+    user: state.user,
+    loggedIn: state.loggedIn,
+  };
 }
